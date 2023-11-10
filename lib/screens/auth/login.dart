20409d0eaef7fd328392a92ee78d3e6ff18a1c5f
Sameer Ghaffar <sameerghaffar19@gmail.com>
2023-11-11 01:32:27 +0500

    Auth Screens slight changes

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -159,8 +159,10 @@ class _LoginScreenState extends State<LoginScreen> {
                       decoration: BoxDecoration(
                         boxShadow: [
                           BoxShadow(
-                            color: const Color(0xff000000).withOpacity(0.1), // Color of the shadow
-                            blurRadius: 4, // Spread of the shadow
+                            color: const Color(0xff000000).withOpacity(0.1),
+                            // Color of the shadow
+                            blurRadius: 4,
+                            // Spread of the shadow
                             offset: const Offset(0, 12),
                           ),
                         ],
